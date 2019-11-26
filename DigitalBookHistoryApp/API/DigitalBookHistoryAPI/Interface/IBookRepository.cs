e552ce8c537efcb98fa0cd59ac2d05c4ecba922d
Josh Wygle <jwygle@gmail.com>
2019-11-25 21:37:09 -0500

    Created beginnings for IBookRepository and BookRepository.

---
+++ DigitalBookHistoryApp/API/DigitalBookHistoryAPI/Interface/IBookRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using DigitalBookHistoryAPI.Models;
+
+namespace DigitalBookHistoryAPI.Interface
+{
+    interface IBookRepository
+    {
+        List<DigitalBook> GetBooks();
+        DigitalBook GetBook(int bookId);
+    }
+}
---
+++ DigitalBookHistoryApp/API/DigitalBookHistoryAPI/Repository/BookRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Options;
+using Dapper;
+using DigitalBookHistoryAPI.Models;
+using DigitalBookHistoryAPI.Settings;
+using DigitalBookHistoryAPI.Interface;
+
+namespace DigitalBookHistoryAPI.Repository
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly AppSettings _appSettings;
+
+        public BookRepository(IOptionsSnapshot<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+
+        public List<DigitalBook> GetBooks()
+        {
+            List<DigitalBook> bookList;
+
+            using (var connection = new SqlConnection(_appSettings.ConnectionStrings.DefaultConnection))
+            {
+                string query = "";
+                bookList = connection.Query<DigitalBook>(query).AsList();
+            }
+
+            return bookList;
+        }
+
+        public DigitalBook GetBook(int Id)
+        {
+            DigitalBook book;
+
+            using (var connection = new SqlConnection(_appSettings.ConnectionStrings.DefaultConnection))
+            {
+                string query = "";
+                book = connection.QueryFirstOrDefault(query);
+            }
+
+            return book;
+        }
+    }
+}
