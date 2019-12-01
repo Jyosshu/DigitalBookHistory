8971f71691221a09f44f2134aa66d9f0f7d7154d
Josh Wygle <jwygle@gmail.com>
2019-12-01 16:03:26 -0500

    Trouble shooting for mac os path

--- DigitalBookHistoryLoader/DigitalBookHistoryLoader/Program.cs
+++ DigitalBookHistoryLoader/DigitalBookHistoryLoader/Program.cs
@@ -4,6 +4,7 @@ using DigitalBookHistoryLoader.repositories;
 using DigitalBookHistoryLoader.models;
 using System.Collections.Generic;
 using System.IO;
+//using Utilities;
 
 namespace DigitalBookHistoryLoader
 {
@@ -15,7 +16,7 @@ namespace DigitalBookHistoryLoader
             string saveLocation = null;
             string fileToRead = null;
             string remoteUrlBase = "https://d2snwnmzyr8jue.cloudfront.net/";
-            List<TitleFields> titleFieldsList = new List<TitleFields>();
+            List<TitleFields> titleFieldsList;
             List<ImageFields> imageFieldsList = new List<ImageFields>();
             IImageRepository imageRepository = new ImageRepository();
             ITitleRepository titleRepository = new TitleRepository();
@@ -25,7 +26,7 @@ namespace DigitalBookHistoryLoader
             {
                 if (args.Length < 2)
                 {
-                    if (args.Length == 1 && args[0] == "q")
+                    if (args.Length == 1 && args[0].ToLower() == "q")
                     {
                         Console.WriteLine("Exiting program.");
                         Environment.Exit(0);
@@ -37,6 +38,7 @@ namespace DigitalBookHistoryLoader
                         Console.WriteLine($"Usage: {Environment.NewLine} \t\"jsonFileToRead\" \"saveLocationForImages\"");
                         Console.WriteLine($"Example: {Environment.NewLine} \t\"C:\\folder\\fileToRead.json\"\"C:\\folder\\folder\\\"");
                         Console.WriteLine("Type q and press Enter to exit program");
+                        args[0] = Console.ReadLine();
                     }
                 }
                 else
@@ -49,6 +51,8 @@ namespace DigitalBookHistoryLoader
 
             Console.WriteLine($"The current base URL for the remote image server == {remoteUrlBase}.  {Environment.NewLine}Is this still correct? (y / n)");
             string choice = Console.ReadLine();
+
+            // User enter new remote image hosting URL
             if (choice.ToLower() == "n" || choice.ToLower() == "no")
             {
                 bool leaveLoop = false;
@@ -77,11 +81,35 @@ namespace DigitalBookHistoryLoader
 
             }
 
+            List<DigitalItem> digitalItems;
 
-            List<DigitalItem> digitalItems = titleRepository.GetHooplaHistory(fileToRead);
+            try
+            {
+                if (!File.Exists(fileToRead))
+                {
+                    throw new FileNotFoundException($"{fileToRead} does not exist.");
+                }
+                else
+                {
+                    digitalItems = titleRepository.GetHooplaHistory(fileToRead);
 
-            titleRepository.LoadDigitalItemsToDb(digitalItems);
+                    titleRepository.LoadDigitalItemsToDb(digitalItems);
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine($"There was an error accessing {fileToRead}.  {ex.Message}. {Environment.NewLine}Press q or x to exit.");
+                var input = Console.ReadKey();
 
+                if (input.ToString().ToUpper() == "Q" || input.ToString().ToUpper() == "X")
+                {
+                    Environment.Exit(1);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"There was an exception.  {ex.Message}");
+            }
 
             if (!Directory.Exists(saveLocation)) Directory.CreateDirectory(saveLocation);
 
@@ -104,6 +132,11 @@ namespace DigitalBookHistoryLoader
             ImageDownloader.DownloadImage(imageFieldsList);
 
             bool imagesInserted = imageRepository.CreateImageFields(imageFieldsList);
+
+            if (imagesInserted == true)
+            {
+                Console.WriteLine("Image records inserted into the database");
+            }
         }
     }
 }
--- postgres_database/user.sql
+++ postgres_database/user.sql
@@ -5,19 +5,19 @@
 CREATE USER digital_book_history_owner WITH PASSWORD 'Noo8nsVSXwzG7WgeArye';
 
 GRANT ALL
-ON ALL TABLES IN SCHEMA public
+ON ALL TABLES IN SCHEMA digital_book_history
 TO digital_book_history_owner;
 
 GRANT ALL
-ON ALL SEQUENCES IN SCHEMA public
+ON ALL SEQUENCES IN SCHEMA digital_book_history
 TO digital_book_history_owner;
 
 CREATE USER digital_book_history_appuser WITH PASSWORD 'AAtjSUbLTRJVbu95LH3T';
 
 GRANT SELECT, INSERT, UPDATE, DELETE
-ON ALL TABLES IN SCHEMA public
+ON ALL TABLES IN SCHEMA digital_book_history
 TO digital_book_history_appuser;
 
 GRANT USAGE, SELECT
-ON ALL SEQUENCES IN SCHEMA public
+ON ALL SEQUENCES IN SCHEMA digital_book_history
 TO digital_book_history_appuser;
